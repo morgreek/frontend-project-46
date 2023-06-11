a3b36509d0ac18f18e7b99f5ebf03d70384dde47
Alexey Sosnin <morgreek@gmail.com>
2023-06-11 14:08:16 +0500

    add option --format without logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
 
