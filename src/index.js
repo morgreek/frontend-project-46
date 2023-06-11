2304c26819ddcb441cae4cff1cda58367fb5f7cd
Alexey Sosnin <morgreek@gmail.com>
2023-06-11 13:47:55 +0500

    preparation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { program } from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program.parse();
+
+export default program;
