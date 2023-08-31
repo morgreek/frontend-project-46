15aed39442ed101b081a5c51703c590ec4799ebd
Alexey Sosnin <morgreek@gmail.com>
2023-08-31 19:31:55 +0500

    fix 3 2023-08-31 19:31

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -16,31 +16,25 @@ const getKeysChain = (parentName, childName) => [parentName, childName]
 const formatter = (diff, name = '') => {
   const result = diff.children.reduce((acc, child) => {
     const paths = getKeysChain(name, child.name);
-
     switch (child.status) {
       case 'tree': {
         return `${acc}${formatter(child, paths)}\n`;
       }
-
       case 'updated': {
         const oldValue = stringify(child.values.old);
         const newValue = stringify(child.values.new);
         return `${acc}Property '${paths}' was updated. From ${oldValue} to ${newValue}\n`;
       }
-
       case 'removed': {
         return `${acc}Property '${paths}' was removed\n`;
       }
-
       case 'added': {
         const value = stringify(child.values.value);
         return `${acc}Property '${paths}' was added with value: ${value}\n`;
       }
-
       case 'same': {
         return acc;
       }
-
       default: {
         throw new Error(`Plain formatter - unknow child status: ${child.status}`);
       }
